8244b8d3543b09243ed95d8c54c0d5c67a11dd3c
Praja S S <71457912+Prajjjah@users.noreply.github.com>
2021-02-27 23:39:01 +0530

    Add files via upload

---
+++ registertest.php
@@ -0,0 +1,36 @@
+<html>
+<body>
+<?php
+session_start();
+$servername="localhost";
+$username="root";
+$password="";
+$dbname="etu_cor";
+$conn=new mysqli($servername,$username,$password,$dbname);
+if (!$conn)
+{
+die('Could not connect: ' . mysql_error());
+}
+if(isset($_POST['signup']))
+{
+$uname=$_POST['username'];
+$pwd=$_POST['password'];
+$email=$_POST['email'];
+$college=$_POST['college'];
+$sql="insert into register(username, emailid, password, college) values('$uname','$email','$pwd','$college')";
+$result=$conn->query($sql);
+$error="Either username or password is incorrect";
+if($result)
+{
+	header('location:hi.html');
+}
+else
+{
+	$_SESSION['register_error']=$error;
+	header('location:login.php');
+}
+}
+$conn->close();
+?>
+</body>
+</html>
